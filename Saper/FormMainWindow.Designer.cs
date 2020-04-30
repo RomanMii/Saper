c6de6134b4d4b88769ed6bcc8d2f49256a8a1e91
ti101 <61845860+ti101@users.noreply.github.com>
2020-04-30 15:45:07 +0200

    Add project files.

---
+++ Saper/FormMainWindow.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Saper
+{
+    partial class FormMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.AutoSize = true;
+            this.panelButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButtons.Location = new System.Drawing.Point(0, 28);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(800, 422);
+            this.panelButtons.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem1,
+            this.normalToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.easyToolStripMenuItem.Text = "dificulty";
+            // 
+            // easyToolStripMenuItem1
+            // 
+            this.easyToolStripMenuItem1.Name = "easyToolStripMenuItem1";
+            this.easyToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.easyToolStripMenuItem1.Text = "easy";
+            this.easyToolStripMenuItem1.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.normalToolStripMenuItem.Text = "normal";
+            this.normalToolStripMenuItem.Click += new System.EventHandler(this.normalToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hardToolStripMenuItem.Text = "hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
+            // FormMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.menuStrip);
+            this.Name = "FormMainWindow";
+            this.Text = "Saper";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+    }
+}
+
---
+++ Saper/FormMainWindow.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Saper
+{
+    public partial class FormMainWindow : System.Windows.Forms.Form
+    {
+        private const int fieldSize = 30;
+        private Logic myGame;
+        public FormMainWindow()
+        {
+            InitializeComponent();
+            easyToolStripMenuItem_Click(null, null);
+        }
+
+        private void easyToolStripMenuItem_Click(object sendert, EventArgs e)
+        {
+            myGame = new Logic(8, 8, 10);
+            generateView();
+        }
+
+        private void normalToolStripMenuItem_Click(object sendert, EventArgs e)
+        {
+            myGame = new Logic(12, 10, 25);
+            generateView();
+        }
+
+        private void hardToolStripMenuItem_Click(object sendert, EventArgs e)
+        {
+            myGame = new Logic(20, 15, 50);
+            generateView();
+        }
+
+        private void generateView()
+        {
+            panelButtons.Controls.Clear();
+
+            for(int x = 0; x < myGame.BoardWidth; x++)
+            {
+                for(int y = 0; y < myGame.BoardHeight; y++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(fieldSize, fieldSize);
+                    b.Location = new Point(fieldSize * x, fieldSize * y);
+                    b.MouseClick += button_Click;
+                    //b.Click -= button_Click;
+                    panelButtons.Controls.Add(b);
+                    b.Tag = new Point(x, y);
+                }
+            }
+        }
+
+        private void button_Click(object sender, MouseEventArgs e)
+        {
+            
+           if (myGame.State == Logic.GameState.InProgress)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    if (sender is Button)
+                    {
+                        Button b = sender as Button;
+                        if (b.Tag is Point)
+                        {
+                            Point p = (Point)b.Tag;
+
+                            myGame.Uncover(p);
+                            refreshView();
+
+                            if (myGame.State == Logic.GameState.Win)
+                            {
+                                MessageBox.Show("Congratulation, you beat me");
+                            }
+                            else if (myGame.State == Logic.GameState.Loss)
+                            {
+                                MessageBox.Show("Ha ha ha, loser!!!");
+                            }
+                        }
+                    }
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    refreshViewRight();
+                }
+            }
+        }
+
+        private void refreshViewRight()
+        {
+            foreach (Button b in panelButtons.Controls)
+            {
+                Logic.Field f = myGame.GetField((Point)b.Tag);
+                if(f.Covered == true)
+                {
+                    b.ForeColor = Color.Yellow;
+                    //b.BackColor = Color.Yellow;
+                    b.Text = "P";
+                }
+            }
+        }
+
+        private void refreshView()
+        {
+            foreach(Button b in panelButtons.Controls)
+            {
+                Logic.Field f = myGame.GetField((Point)b.Tag);
+                if(f.Covered == false)
+                {
+                    if (f.FieldType == Logic.FieldTypeEnum.Bomb)
+                    {
+                        b.BackColor = Color.Red;
+                        b.Text = "#";
+                    }
+                    else
+                    {
+                        b.BackColor = Color.White;
+                        if(f.FieldType == Logic.FieldTypeEnum.BombNumber)
+                        {
+                            b.Text = f.BombCount.ToString();
+                        }
+                    }   
+                }
+            }
+        }
+    }
+}
